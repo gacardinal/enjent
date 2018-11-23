ecf4e354a4ff9c027abd74fc3631f438da99b662
Gabriel Cardinal <gabrielc@outlook.fr>
2018-11-23 11:37:47 -0500

    Implemented events for WebSocketServer

---
+++ net/WebSocket/WebSocketServerEvents.cs
@@ -0,0 +1,21 @@
+namespace NarcityMedia.Net
+{
+    public partial class WebSocketServer
+    {
+        public event WebSocketServerEvent OnConnect;
+        public event WebSocketServerEvent OnDisconnect;
+        public event WebSocketServerEvent OnMessage;
+
+        public delegate void WebSocketServerEvent(object sender, WebSocketServerEventArgs a);
+
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WebSocketServerEventArgs
+    {
+        
+    }
+}
