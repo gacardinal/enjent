8ab8b29e8bb60817aa40cac7e54a0df23af2fca6
Gabriel Cardinal <gabrielc@outlook.fr>
2019-01-10 17:42:28 -0500

    Loop over explicit .ToList() to ensure a stable copy

--- net/WebSocket/WebSocketPool.cs
+++ net/WebSocket/WebSocketPool.cs
@@ -7,6 +7,97 @@ using System.Linq;
 
 namespace NarcityMedia.Net
 {
+
+    /// <summary>
+    /// Maintains a collection of WebSOcketPool objects.
+    /// Each WebSocketPool within the WebSocketPoolManager gets assigned a Thread that will constantly
+    /// check the buffer of each WebSocket within the WebSocketPool and takes actions according to the state of the buffer.
+    /// The WebSocketPoolManager ensures that all WebSocketPools contain a balanced number of WebSockets to maintain
+    /// consistent performances.
+    /// </summary>
+    internal class WebSocketPoolManager
+    {
+        private const int INITIAL_POOL_COUNT = 2;
+        private List<WebSocketPool> socketPools;
+        private List<ClientPoolAssoc> clientPoolsAssociations;
+
+        public event FrameHandler OnManagerFrame;
+        public delegate void FrameHandler(WebSocketClient cli, SocketFrame frame);
+
+        public WebSocketPoolManager(FrameHandler f)
+        {
+            this.socketPools = new List<WebSocketPool>(INITIAL_POOL_COUNT);
+            this.clientPoolsAssociations = new List<ClientPoolAssoc>(INITIAL_POOL_COUNT * 1024);
+            this.OnManagerFrame += f;
+            for (int i = 0; i < INITIAL_POOL_COUNT; i++)
+            {
+                WebSocketPool pool = new WebSocketPool(i);
+                pool.OnPoolFrame += this.FrameHandlerCallback;
+                this.socketPools.Add(pool);
+                pool.StartListening();
+            }
+        }
+
+        /// <summary>
+        /// Callback that is executed when a WebSocketPool parses a WebSocketFrame comming from a client
+        /// </summary>
+        /// <param name="cli">The client that sent the frame</param>
+        /// <param name="frame">The frame that was sent</param>
+        /// <remarks>
+        /// </remarks>
+        private void FrameHandlerCallback(WebSocketClient cli, SocketFrame frame)
+        {
+            foreach (WebSocketPool pool in this.socketPools)
+            {
+                Console.WriteLine("Pool " + pool.POOL_ID + " has " + pool.clients.Count + " clients");
+            }
+
+            this.OnManagerFrame.Invoke(cli, frame);
+        }
+
+        /// <summary>
+        /// Inserts a client object in a WebSocketPool's socket collection and always inserts the given
+        /// client object in the pool that contains the less elements to keep the pools balanced
+        /// </summary>
+        /// <param name="cli">The client object to insert</param>
+        public void AddClient(WebSocketClient cli)
+        {
+            WebSocketPool pool = this.socketPools.Aggregate((a, b) => a.clients.Count < b.clients.Count ? a : b);
+            if (pool != null) {
+                this.clientPoolsAssociations.Add(new ClientPoolAssoc(cli, pool));
+                pool.AddClient(cli);
+            }
+        }
+
+        /// <summary>
+        /// Locates and removes a given client from its thread pool
+        /// </summary>
+        /// <param name="cli">The client object to remove</param>
+        /// <returns>Whether the remove operation was successful</returns>
+        public bool RemoveClient(WebSocketClient cli)
+        {
+            ClientPoolAssoc cliAssoc = this.clientPoolsAssociations.Find(x => x.client == cli);
+            this.clientPoolsAssociations.Remove(cliAssoc);
+            return cliAssoc.pool.RemoveClient(cli);
+        }
+
+        /// <summary>
+        /// Used to associate a client object with the websocketpool it's in to avoid having to loop through
+        /// all of the websocketpools to find or delete a client object
+        /// </summary>
+        private struct ClientPoolAssoc
+        {
+            public WebSocketClient client;
+            public WebSocketPool pool;
+
+            public ClientPoolAssoc(WebSocketClient client, WebSocketPool pool)
+            {
+                this.client = client;
+                this.pool = pool;
+            }
+        }
+    }
+    
     /// <summary>
     /// An instance of WebSocketPool, not to be confused with a Room, is used to regroup sockets together so a
     /// Thread can iterate over them and listen to them all.
@@ -54,7 +145,7 @@ namespace NarcityMedia.Net
             {
                 lock (this.clients)
                 {
-                    foreach (WebSocketClient cli in this.clients)
+                    foreach (WebSocketClient cli in this.clients.ToList())
                     {
                         // WebSocket frames are 2 bytes minimum
                         if (cli.socket.Available >= 2)
@@ -127,95 +218,6 @@ namespace NarcityMedia.Net
             lock (this.clients)
             {
                 return this.clients.Remove(cli);
-            }
-        }
-    }
-
-    /// <summary>
-    /// Maintains a collection of WebSOcketPool objects.
-    /// Each WebSocketPool within the WebSocketPoolManager gets assigned a Thread that will constantly
-    /// check the buffer of each WebSocket within the WebSocketPool and takes actions according to the state of the buffer.
-    /// The WebSocketPoolManager ensures that all WebSocketPools contain a balanced number of WebSockets to maintain
-    /// consistent performances.
-    /// </summary>
-    internal class WebSocketPoolManager
-    {
-        private const int INITIAL_POOL_COUNT = 2;
-        private List<WebSocketPool> socketPools;
-        private List<ClientPoolAssoc> clientPoolsAssociations;
-
-        public event FrameHandler OnManagerFrame;
-        public delegate void FrameHandler(WebSocketClient cli, SocketFrame frame);
-
-        public WebSocketPoolManager(FrameHandler f)
-        {
-            this.socketPools = new List<WebSocketPool>(INITIAL_POOL_COUNT);
-            this.clientPoolsAssociations = new List<ClientPoolAssoc>(INITIAL_POOL_COUNT * 1024);
-            this.OnManagerFrame += f;
-            for (int i = 0; i < INITIAL_POOL_COUNT; i++)
-            {
-                WebSocketPool pool = new WebSocketPool(i);
-                pool.OnPoolFrame += this.FrameHandlerCallback;
-                this.socketPools.Add(pool);
-                pool.StartListening();
-            }
-        }
-
-        /// <summary>
-        /// Callback that is executed when a WebSocketPool parses a WebSocketFrame comming from a client
-        /// </summary>
-        /// <param name="cli">The client that sent the frame</param>
-        /// <param name="frame">The frame that was sent</param>
-        /// <remarks>
-        /// </remarks>
-        private void FrameHandlerCallback(WebSocketClient cli, SocketFrame frame)
-        {
-            foreach (WebSocketPool pool in this.socketPools)
-            {
-                Console.WriteLine("Pool " + pool.POOL_ID + " has " + pool.clients.Count + " clients");
-            }
-            this.OnManagerFrame.Invoke(cli, frame);
-        }
-
-        /// <summary>
-        /// Inserts a client object in a WebSocketPool's socket collection and always inserts the given
-        /// client object in the pool that contains the less elements to keep the pools balanced
-        /// </summary>
-        /// <param name="cli">The client object to insert</param>
-        public void AddClient(WebSocketClient cli)
-        {
-            WebSocketPool pool = this.socketPools.Aggregate((a, b) => a.clients.Count < b.clients.Count ? a : b);
-            if (pool != null) {
-                this.clientPoolsAssociations.Add(new ClientPoolAssoc(cli, pool));
-                pool.AddClient(cli);
-            }
-        }
-
-        /// <summary>
-        /// Locates and removes a given client from its thread pool
-        /// </summary>
-        /// <param name="cli">The client object to remove</param>
-        /// <returns>Whether the remove operation was successful</returns>
-        public bool RemoveClient(WebSocketClient cli)
-        {
-            ClientPoolAssoc cliAssoc = this.clientPoolsAssociations.Find(x => x.client == cli);
-            this.clientPoolsAssociations.Remove(cliAssoc);
-            return cliAssoc.pool.RemoveClient(cli);
-        }
-
-        /// <summary>
-        /// Used to associate a client object with the websocketpool it's in to avoid having to loop through
-        /// all of the websocketpools to find or delete a client object
-        /// </summary>
-        private struct ClientPoolAssoc
-        {
-            public WebSocketClient client;
-            public WebSocketPool pool;
-
-            public ClientPoolAssoc(WebSocketClient client, WebSocketPool pool)
-            {
-                this.client = client;
-                this.pool = pool;
             }
         }
     }
