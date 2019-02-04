bb19ccde5ece7b3dfc1b4d92be287bd28f638eb2
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-02-04 17:31:46 -0500

    Added rudimentary HTTPRequest object that represents the initial HTTP request for the upgrade

---
+++ net/HTTPRequest.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace NarcityMedia.Net
+{
+    /// <summary>
+    /// Represents supported HTTP methods
+    /// </summary>
+    public enum HTTPMethod {
+        GET, POST, DELETE, PUT
+    }
+
+    public class HTTPRequest
+    {
+        public string URL;
+        public Dictionary<string, string> Headers;
+        public HTTPMethod Methods;
+        
+        public HTTPRequest(string url, HTTPMethod method, Dictionary<string, string> headers)
+        {
+            this.URL = url;
+            this.Methods = method;
+            this.Headers = headers;
+        }
+
+        public HTTPRequest(string url, HTTPMethod method, Dictionary<string, byte[]> headers)
+        {
+            this.URL = url;
+            this.Methods = method;
+
+            Dictionary<string, string> mappedHeaders = new Dictionary<string, string>(headers.Count);
+            foreach (KeyValuePair<string, byte[]> header in headers)
+            {
+                mappedHeaders.Add(header.Key, System.Text.Encoding.Default.GetString(header.Value));
+            }
+
+            this.Headers = mappedHeaders;
+        }
+    }
+}
