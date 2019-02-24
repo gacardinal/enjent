9dfebdb1ff1292beb3d564c10d48dcb7063dcd5a
Gabriel Cardinal <gabrielc@outlook.fr>
2019-02-24 14:29:38 -0500

    WebSocket close status enumerator

--- net/WebSocket/WebSocket.cs
+++ net/WebSocket/WebSocket.cs
@@ -10,7 +10,6 @@ namespace NarcityMedia.Net
     /// </summary>
     public abstract partial class SocketFrame
     {
-
         /// <summary>
         /// Enumerates the possible OPCodes of a WebSocket frame as described in RFC 6455
         /// </summary>
@@ -45,6 +44,89 @@ namespace NarcityMedia.Net
             /// Indicates a PONG frame
             /// </summary>
             Pong = 0xA
+        }
+
+        public enum CloseCodes
+        {
+            /// <summary>
+            /// Indicates a normal closure, meaning that the purpose for
+            /// which the connection was established has been fulfilled.
+            /// </summary>
+            NormalClosure = 1000,
+
+            /// <summary>
+            /// Indicates that an endpoint is "going away", such as a server
+            /// going down or a browser having navigated away from a page.
+            /// </summary>
+            GoingAway = 1001,
+        
+            /// <summary>
+            /// Indicates that an endpoint is terminating the connection due
+            /// to a protocol error.
+            /// </summary>
+            ProtocolError = 1002,
+
+            /// <summary>
+            /// Indicates that an endpoint is terminating the connection
+            /// because it has received a type of data it cannot accept (e.g., an
+            /// endpoint that understands only text data MAY send this if it
+            /// receives a binary message).
+            /// </summary>
+            UnacceptableDataType = 1003,
+
+            // 1004 is not defined yet
+
+            // 1005 Is a reserved value
+
+            // 1006 is a reserved value
+
+            /// <ummary>
+            /// Indicates that an endpoint is terminating the connection
+            /// because it has received data within a message that was not
+            /// consistent with the type of the message (e.g., non-UTF-8 [RFC3629]
+            /// data within a text message).
+            /// </summary>
+            InconsistentDataType = 1007,
+
+            /// <summary>
+            /// Indicates that an endpoint is terminating the connection
+            /// because it has received a message that violates its policy.  This
+            /// is a generic status code that can be returned when there is no
+            /// other more suitable status code (e.g., 1003 or 1009) or if there
+            /// is a need to hide specific details about the policy.
+            /// </summary>
+            PolicyViolation = 1008,
+
+            /// <summary>
+            /// Indicates that an endpoint is terminating the connection
+            /// because it has received a message that is too big for it to
+            /// process.
+            /// </summary>
+            MessageSizeExceeded = 1009,
+
+            /// <summary>
+            /// dicates that an endpoint (client) is terminating the
+            /// connection because it has expected the server to negotiate one or
+            /// more extension, but the server didn't return them in the response
+            /// message of the WebSocket handshake.  The list of extensions that
+            /// are needed SHOULD appear in the /reason/ part of the Close frame.
+            /// Note that this status code is not used by the server, because it
+            /// can fail the WebSocket handshake instead.
+            /// </summary>
+            ExtensionNegotiationFailure = 1010,
+
+            /// <summary>
+            /// ndicates that a server is terminating the connection because
+            /// it encountered an unexpected condition that prevented it from
+            /// fulfilling the request.
+            /// </summary>
+            UnexpectedCondition = 1011
+
+            // 1012 is undefined
+            // 1013 is undefined
+            // 1014 is undefined
+
+            // 1015 is a reserved value
         }
 
         /// <summary>
