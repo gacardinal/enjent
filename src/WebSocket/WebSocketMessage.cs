3fa201218f31fb03b22f09a7da243b5417420a83
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-12-26 23:19:48 -0500

    WebSocketMessage abstract class with concrete implementations for text and binary

--- src/WebSocket/WebSocketMessage.cs
+++ src/WebSocket/WebSocketMessage.cs
@@ -1,45 +1,21 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 
 namespace NarcityMedia.Enjent
 {
-	public abstract class WebSocketMessage
+	public abstract class WebSocketMessage<TFrameType> where TFrameType : WebSocketDataFrame, new()
 	{
 		public WebSocketDataType DataType;
 
-		public abstract IEnumerable<WebSocketDataFrame> GetFrames();
-	}
-	
-    /// <summary>
-    /// Represents a message that is to be sent via WebSocket.
-    /// A message is composed of one or more frames.
-    /// </summary>
-    /// <remarks>This public class only support messages that can fit in a single frame for now</remarks>
-    public class BinaryMessage
-    {
-		/// <summary>
-		/// Represents the type of data contained in the current message
-		/// </summary>
-		public readonly WebSocketDataType DataType = WebSocketDataType.Binary;
+		public byte[] Payload;
 
-		/// <summary>
-		/// Creates an instance of WebSocketMessage that contains the given payload.
-		/// <see cref="MessageType" /> will be set to <see cref="WebSocketDataFrame.DataFrameType.Binary" />
-		/// </summary>
-		/// <param name="payload">The payload to initialize the message with</param>
-        public BinaryMessage(byte[] payload)
-        {
-			this.DataType = WebSocketDataType.Binary;
-		}
+		public WebSocketMessage() : this(new byte[0])
+		{}
 
-		/// <summary>
-		/// Creates an instance of WebSocketMessage that contains the given message as payload.
-		/// <see cref="MessageType" /> will be set to <see cref="WebSocketDataFrame.DataFrameType.Text" />
-		/// </summary>
-		/// <param name="message">The message to set as the payload</param>
-		public BinaryMessage(string message)
+		public WebSocketMessage(byte[] payload)
 		{
-			this.DataType = WebSocketDataType.Text;
+			this.Payload = payload;
 		}
 
         /// <summary>
@@ -48,13 +24,59 @@ namespace NarcityMedia.Enjent
         /// </summary>
         /// <remarks>The method currently supports only 1 frame messages</remarks>
         /// <returns>A List containing the frames of the message</returns>
-        public List<WebSocketBinaryFrame> GetFrames()
+        public IEnumerable<TFrameType> GetFrames(bool masked = false)
         {
-            List<WebSocketBinaryFrame> frames = new List<WebSocketBinaryFrame>(1);
+            TFrameType[] frames = new TFrameType[1];
 			
-            frames.Add(new WebSocketBinaryFrame(true, false, this.Payload));
+            TFrameType frame = new TFrameType();
+			frame.Fin = true;
+			frame.Masked = masked;
 
             return frames;
         }
+	}
+	
+    /// <summary>
+    /// Represents a message that is to be sent via WebSocket.
+    /// A message is composed of one or more frames.
+    /// </summary>
+    /// <remarks>This public class only support messages that can fit in a single frame for now</remarks>
+    public class BinaryMessage : WebSocketMessage<WebSocketBinaryFrame>
+    {
+		/// <summary>
+		/// Creates an instance of WebSocketMessage that contains the given payload.
+		/// <see cref="MessageType" /> will be set to <see cref="TF.DataFrameType.Binary" />
+		/// </summary>
+		/// <param name="payload">The payload to initialize the message with</param>
+        public BinaryMessage(byte[] payload) : base(payload)
+        {
+			this.DataType = WebSocketDataType.Binary;
+		}
     }
+
+	public class TextMessage : WebSocketMessage<WebSocketTextFrame>
+	{
+		private string _message;
+
+		public string Message
+		{
+			get { return _message; }
+			set
+			{
+				this._message = value;
+				this.Payload = Encoding.UTF8.GetBytes(value);
+			}
+		}
+
+		public TextMessage(byte[] bytes) : base(bytes)
+		{
+			this._message = Encoding.UTF8.GetString(bytes);
+		}
+
+		public TextMessage(string message)
+		{
+			this._message = message;
+			this.Payload = Encoding.UTF8.GetBytes(message);
+		}
+	}
 }
