648a0cc1fa6d43f3953cfe03abc25bee8d8a78c7
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-12-29 03:20:39 -0500

    Base test class for convenience

---
+++ unitTests/EnjentTest.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit.Abstractions;
+
+namespace EnjentUnitTests
+{
+
+    /// <summary>
+	/// Base class for XUnit tests for Enjent
+    /// </summary>
+    public abstract class EnjentTest
+    {
+    	private readonly ITestOutputHelper output;
+
+		private static Random rand = new Random();
+
+		public EnjentTest(ITestOutputHelper o)
+		{
+			this.output = o;
+		}
+
+		public void Log(string message)
+		{
+			if (this.output != null)
+			{
+				this.output.WriteLine(message);
+			}
+		}
+	}
+}
