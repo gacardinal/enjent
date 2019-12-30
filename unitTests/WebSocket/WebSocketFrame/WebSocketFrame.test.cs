55535c43ccb5398a09ae71afad09e0a0ec40b14e
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-12-29 20:36:30 -0500

    Abstracted frame comparison logic for tests

--- unitTests/WebSocket/WebSocketFrame/WebSocketFrame.test.cs
+++ unitTests/WebSocket/WebSocketFrame/WebSocketFrame.test.cs
@@ -14,56 +14,34 @@ namespace EnjentUnitTests.WebSocket
     /// </summary>
     public partial class WebSocketFrameTest : EnjentTest
     {
-        private static Random rand = new Random();
 
 		public WebSocketFrameTest(ITestOutputHelper o) : base(o)
 		{}
 
 		[Fact]
-		public void WebSocketFrame_FIN_Init()
-		{
-			WebSocketFrameConcretion f = new WebSocketFrameConcretion(false, false, new byte[0]);
-			Assert.True(f.Fin == false, "WebSocketFrame.Fin was not correctly initialized");
-
-			WebSocketFrameConcretion f2 = new WebSocketFrameConcretion(true, false, new byte[0]);
-			Assert.True(f2.Fin == true, "WebSocketFrame.Fin was not correctly initialized");
-		}
-
-		[Fact]
-		public void WebSocketFrame_MASKED_Init()
-		{
-			WebSocketFrameConcretion f = new WebSocketFrameConcretion(false, false, new byte[0]);
-			Assert.True(f.Masked == false, "WebSocketFrame.Masked was not correctly initialized");
-
-			WebSocketFrameConcretion f2 = new WebSocketFrameConcretion(false, true, new byte[0]);
-			Assert.True(f2.Masked == true, "WebSocketFrame.Masked was not correctly initialized");
-		}
-
-		[Fact]
 		public void WebSocketFrame_PAYLOAD_Init()
 		{
-			WebSocketFrameConcretion f = new WebSocketFrameConcretion(false, false, new byte[0]);
+			WebSocketFrameConcretion f = new WebSocketFrameConcretion(new byte[0]);
 
 			byte[] oneBytePayload = new byte[1];
 			rand.NextBytes(oneBytePayload);
-			WebSocketFrameConcretion f2 = new WebSocketFrameConcretion(false, false, oneBytePayload);
+			WebSocketFrameConcretion f2 = new WebSocketFrameConcretion(oneBytePayload);
 
 			byte[] ratherLargePayload = new byte[256];
 			rand.NextBytes(ratherLargePayload);
-			WebSocketFrameConcretion f3 = new WebSocketFrameConcretion(false, false, ratherLargePayload);
+			WebSocketFrameConcretion f3 = new WebSocketFrameConcretion(ratherLargePayload);
 			
 			Assert.True(f.Payload.SequenceEqual(new byte[0]), "WebSocketDataFrame payload was not initialized correctly");
 			Assert.True(f2.Payload.SequenceEqual(oneBytePayload), "WebSocketDataFrame payload was not initialized correctly");
 			Assert.True(f3.Payload.SequenceEqual(ratherLargePayload), "WebSocketDataFrame payload was not initialized correctly");
 		}
 
-
 		[Fact]
 		public void WebSocketFrame_NULL_PAYLOAD_Init()
 		{
-			WebSocketFrameConcretion f = new WebSocketFrameConcretion(false, false, null);
-
-			Assert.True(new byte[0].SequenceEqual(f.Payload), "WebSOcketFrame received null as payload constructor argument but did not default Payload to empty byte array");
+			Assert.Throws<ArgumentNullException>("payload", () => {
+				WebSocketFrameConcretion f = new WebSocketFrameConcretion(null);
+			});
 		}
 
         [Fact]
@@ -120,64 +98,19 @@ namespace EnjentUnitTests.WebSocket
         public void WebSocketTextFrame_GetBytes(WebSocketFrame frame)
         {
 			byte[] frameBytes = frame.GetBytes();
-
 			WebSocketFrame parsedFrame = WebSocketFrame.Parse(new MemoryStream(frameBytes));
-
-			Assert.Equal(frame.Fin, parsedFrame.Fin);
-			Assert.Equal(frame.Masked, parsedFrame.Masked);
-			Assert.Equal(frame.OpCode, parsedFrame.OpCode);
-			Assert.Equal(frame.Payload, parsedFrame.Payload);
-			if (frame.Masked)
-			{
-				Assert.Equal(frame.MaskingKey, parsedFrame.MaskingKey);
-			}
-
-			// byte[] frameBytes = frame.GetBytes();
-
-			// bool actualFin = (byte)(frameBytes[0] >> 7) == 1;
-			// WebSocketOPCode actualOpCode = (WebSocketOPCode)(frameBytes[0] & 0b00001111);
-			// bool actualMasked = (frameBytes[1] >> 7) == 1;
-
-			// Assert.True(actualFin == frame.Fin, "Frame FIN bit was not set properly");
-			// Assert.True(actualOpCode == frame.OpCode, "Frame OPCode bits were not set properly");
-			// Assert.True(actualMasked == frame.Masked, "Frame MASKED bit was not set properly");
-
-			// byte l = (byte)(frameBytes[1] & 0b01111111);
-			// int frameHeaderSize = 2;
-			// int length;
-			// if (l <= 125)
-			// {
-			// 	length = l;
-			// }
-			// else if (l <= 126)
-			// {
-			// 	byte[] lengthBytes = new byte[2];
-			// 	frameHeaderSize = frameHeaderSize + 2;
-			// 	Array.Copy(frameBytes, 2, lengthBytes, 0, 2);
-			// 	lengthBytes.ReverseIfLittleEndian();
-			// 	length = BitConverter.ToUInt16(lengthBytes, 0);
-			// }
-			// else
-			// {
-			// 	byte[] lengthBytes = new byte[4];
-			// 	frameHeaderSize = frameHeaderSize + 4;
-			// 	Array.Copy(frameBytes, 2, lengthBytes, 0, 4);
-			// 	lengthBytes.ReverseIfLittleEndian();
-			// 	length = (int)BitConverter.ToUInt32(lengthBytes);
-			// }
-
-			// byte[] maskingKey = new byte[4];
-			// if (actualMasked)
-			// {
-			// 	Array.Copy(frameBytes, frameHeaderSize, maskingKey, 0, 4);
-			// }
-
-			// Assert.True(length == frame.Payload.Length, "Encoded payload length did not match actual payload length");
-			// Assert.True(frameBytes.Length == frameHeaderSize + length + (actualMasked ? maskingKey.Length : 0), "Frame object did not return the correct number of bytes");
-
-			// byte[] actualContent = new byte[length];
-			// Array.Copy(frameBytes, frameHeaderSize + (actualMasked ? maskingKey.Length : 0), actualContent, 0, length);
-			// Assert.True(Enumerable.SequenceEqual(actualMasked ? WebSocketFrame.ApplyMask(frame.Payload, maskingKey) : frame.Payload, actualContent), "'Real' frame payload did not match the frame's payload property");
         }
+
+		public void CompareFrames(WebSocketFrame a, WebSocketFrame b)
+		{
+			Assert.Equal(a.Fin, b.Fin);
+			Assert.Equal(a.Masked, b.Masked);
+			Assert.Equal(a.OpCode, b.OpCode);
+			Assert.Equal(a.Payload, b.Payload);
+			if (a.Masked)
+			{
+				Assert.Equal(a.MaskingKey, b.MaskingKey);
+			}
+		}
 	}
 }
