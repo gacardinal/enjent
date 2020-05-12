df8585e0dc790a30d6f225a53cd829c54c1429ed
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2020-05-12 11:42:57 -0400

    Increased max header length to 4MB

--- src/Server/Negociator.cs
+++ src/Server/Negociator.cs
@@ -39,7 +39,7 @@ namespace NarcityMedia.Enjent.Server
         /// <summary>
         /// The maximum accepted header size for the initial HTTP request
         /// </summary>
-        private const int MAX_REQUEST_HEADERS_LENGTH = 1024 * 2;
+        private const int MAX_REQUEST_HEADERS_LENGTH = 1024 * 4;
 
         /// <summary>
         /// Sec-WebSocket-Key HTTP header name
